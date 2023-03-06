61a831367aa80fbe78755ec645a33ef285e6c02a
Ilya Dvorchuk <iladvorchuk@gmail.com>
2023-03-06 22:37:17 +0200

    feat: разрешение корсам

--- index.js
+++ index.js
@@ -35,7 +35,10 @@ const app = express()
 
 // const upload = multer({storage})
 
-app.use(cors())
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+}))
 app.use(cookieParser())
 app.use(express.json())
 app.use('/uploads', express.static('uploads'))
